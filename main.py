3b58511d7b304055e91cf62db9d8bfc398755d44
lukasurmava <surmava0@gmail.com>
2025-04-01 23:18:44 +0400

    Added todo delete

--- main.py
+++ main.py
@@ -104,3 +104,13 @@ def update_todo(todo_id: int, todo: TodoUpdate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_todo)
     return db_todo
+
+# Delete a todos by ID
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not db_todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.delete(db_todo)
+    db.commit()
+    return {"detail": "Todo deleted"}
