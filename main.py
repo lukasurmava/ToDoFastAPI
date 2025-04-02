d2ac310a50aa7d4b5100569b6aa0dd9d24efaac5
lukasurmava <surmava0@gmail.com>
2025-04-02 19:27:49 +0400

    Added checks and refactored

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from idlelib.query import Query
-
 from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-
-from database import StatusEnum, PriorityEnum
+from database import PriorityEnum
 from schemas import UserCreate, UserResponse, UserUpdate, TodoResponse, TodoCreate, TodoUpdate
-from typing import List, Optional
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.pool import StaticPool
+from typing import List
+from sqlalchemy.orm import Session
 from database import get_db, seed_db, User, Todo
 
 
@@ -27,6 +20,8 @@ def on_startup():
 # Create a new user
 @app.post("/users/", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    if not user.username:
+        raise HTTPException(status_code=422, detail="Username can't be empty!")
     db_user = User(username=user.username, email=user.email)
     db.add(db_user)
     db.commit()
@@ -52,8 +47,9 @@ def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.id == user_id).first()
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
-    if user.username is not None:
-        db_user.username = user.username
+    if not user.username:
+        raise HTTPException(status_code=422, detail="Username can't be empty!")
+    db_user.username = user.username
     if user.email is not None:
         db_user.email = user.email
     db.commit()
@@ -73,6 +69,10 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
 # Create a new todos
 @app.post("/todos/", response_model=TodoResponse)
 def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
+    if not todo.title:
+        raise HTTPException(status_code=422, detail="Title can't be empty!")
+    if not db.query(User).filter(User.id == todo.user_id).first():
+        raise HTTPException(status_code=404, detail="User by this id is not found!")
     db_todo = Todo(title=todo.title, description=todo.description, status=todo.status, priority=todo.priority, user_id=todo.user_id)
     db.add(db_todo)
     db.commit()
@@ -98,6 +98,10 @@ def update_todo(todo_id: int, todo: TodoUpdate, db: Session = Depends(get_db)):
     db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
     if not db_todo:
         raise HTTPException(status_code=404, detail="Todo not found")
+    if not todo.title:
+        raise HTTPException(status_code=422, detail="title can't be empty!")
+    if not db.query(User).filter(User.id == todo.user_id):
+        raise HTTPException(status_code=404, detail="User not found")
     db_todo.title = todo.title
     db_todo.description = todo.description
     db_todo.status = todo.status
@@ -120,12 +124,22 @@ def delete_todo(todo_id: int, db: Session = Depends(get_db)):
 # Get todos by user
 @app.get("/todos/todobyuser/{user_id}", response_model=List[TodoResponse])
 async def get_todo_by_user(user_id: int, db: Session = Depends(get_db)):
+    if not db.query(User).filter(User.id == user_id):
+        raise HTTPException(status_code=404, detail="User not found")
     return db.query(Todo).filter(Todo.user_id == user_id).all()
 
 @app.get("/todos/{user_id}/todobyuser/{todo_id}", response_model=TodoResponse)
 async def get_todo_by_todoid_userid(user_id: int, todo_id: int, db: Session = Depends(get_db)):
-    return db.query(Todo).filter(Todo.user_id == user_id, Todo.id == todo_id).first()
+    if not db.query(User).filter(User.id == user_id):
+        raise HTTPException(status_code=404, detail="User not found")
+    todo = db.query(Todo).filter(Todo.user_id == user_id, Todo.id == todo_id).first()
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return todo
 
 @app.get("/todos/getbypriority/", response_model=List[TodoResponse])
 async def get_todo_by_priority(priority: PriorityEnum, db: Session = Depends(get_db)):
-    return db.query(Todo).filter(Todo.priority == priority.value).all()
+    db_todo = db.query(Todo).filter(Todo.priority == priority.value).all()
+    if not db_todo:
+        raise HTTPException(status_code=404, detail="Todos with this priority was not found")
+    return db_todo
