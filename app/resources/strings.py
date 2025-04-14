54a59f880cb5433321f8c0405b03ace65852acd9
lukasurmava <surmava0@gmail.com>
2025-04-14 17:53:25 +0400

    Added messages for exceptions

---
+++ app/resources/strings.py
@@ -0,0 +1,10 @@
+# API messages
+
+
+USER_NOT_FOUND = "User not found"
+USERNAME_CANT_BE_EMPTY = "Username can't be empty!"
+USER_DELETED = "User deleted"
+TITLE_CANT_BE_EMPTY = "Title can't be empty!"
+TODO_NOT_FOUND = "Todo not found"
+NO_TODO_FOR_THIS_USER = "No todo found for this user!"
+TODOS_WITH_THIS_PRIORITY_NOT_FOUND = "Todos with this priority was not found"
